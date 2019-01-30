aaa43a8d5a37437dacca699f29ba860c642a34d0
ireneacevedo <irene.acevedo.2000@gmail.com>
2019-01-30 19:20:30 +0100

    Extra P0

---
+++ P0-extra/CPLX2_count.py
@@ -0,0 +1,26 @@
+with open ('CPLX2.txt','r') as f:
+    sequence = f.read().partition('\n')
+    seq = sequence[2].replace ('\n','')
+
+    countA = 0
+    countC = 0
+    countT = 0
+    countG = 0
+
+    for letter in seq:
+        if letter == 'A':
+            countA += 1
+        if letter == 'C':
+            countC += 1
+        if letter == 'T':
+            countT += 1
+        if letter == 'G':
+            countG += 1
+if letter != 'A' and letter != 'C' and letter != 'G' and letter != 'T':
+    print('The sequence has no A,C,G or T')
+
+print('Total length:{}'.format(length))
+print('A:{}'.format(countA))
+print('C:{}'.format(countC))
+print('T:{}'.format(countT))
+print('G:{}'.format(countG))
---
+++ P0-extra/CPLX2_print.py
@@ -0,0 +1,2 @@
+with open ('CPLX2.txt' , 'r') as f:
+    print(f.read())

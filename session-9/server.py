e371e81aa0fbd1a17d1128dddf055656a08ecee3
ireneacevedo <irene.acevedo.2000@gmail.com>
2019-02-19 16:03:32 +0100

    server

---
+++ session-9/server.py
@@ -0,0 +1,41 @@
+import socket
+
+PORT  = 8080
+IP = '212.128.253.111'
+MAX_OPEN_REQUEST = 5
+
+def process_client(cs):
+
+    # Reading the message from the client
+    msg = cs.recv(2048).decode('utf-8')
+
+    print('Message from the client: {}'.format(msg)
+
+    # Sending the message back to the client
+    # because we are an echo server
+
+    cs.send(str.encode(msg))
+
+    cs.close()
+
+# Create a socket for connecting to the clients
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+serversocket.bind((IP, PORT))
+
+serversocket.listen(MAX_OPEN_REQUEST)
+
+print('Socket ready : {}'.format(serversocket))
+
+while True:
+
+    print('Waiting for connections at : {}, {}'.format(IP, PORT))
+    (clientsocket, adress) = serversocket.accept()
+
+    # --- Process the client request
+    print('Attending client: {}'.format(adress))
+
+    process_client(clientsocket)
+
+    # --- Close the socket
+    clientsocket.close()

79d1cb8b0936e8be3abfb6ee5a1f63c33c7508fc
ireneacevedo <irene.acevedo.2000@gmail.com>
2019-02-19 15:44:33 +0100

    client

---
+++ session-9/client.py
@@ -0,0 +1,30 @@
+
+
+import socket
+
+# SERVER IP, PORT
+IP = "192.168.1.36"
+PORT = 8087
+
+
+
+while True:
+
+    # The client is blocking the server....  NOT A GOOD DESIGN!!!
+    msg = input('>')
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # establish the connection to the Server (IP, PORT)
+    s.connect((IP, PORT))
+
+    # Send the request message to the server
+    s.send(str.encode(msg))
+
+    # Receive the servers respoinse
+    response = s.recv(2048).decode()
+
+    # Print the server's response
+    print("Response: {}".format(response))
+
+    s.close()
